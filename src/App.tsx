4510993f62940d0542a66c8f0eb904d3a839008c
Thanh Vu <thanh@putatu.com>
2019-06-04 23:31:54 +0700

    Create the first typescript component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./components/Hello";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Hello name="thanhdaica" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -21,6 +23,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
