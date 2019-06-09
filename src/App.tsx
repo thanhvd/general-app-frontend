433a49225bf192e738bcd55cbee72c929823004a
Thanh Vu <thanh@putatu.com>
2019-06-09 23:15:11 +0700

    Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  RouteComponentProps
+} from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Hello from "./containers/Hello";
 
-const App: React.FC = () => {
+function Index() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +28,37 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
+  );
+}
+
+type TParams = { id: string };
+
+function Product({ match }: RouteComponentProps<TParams>) {
+  return <h2>This is a page for product with ID: {match.params.id} </h2>;
+}
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products/1">First Product</Link>
+            </li>
+            <li>
+              <Link to="/products/2">Second Product</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/products/:id" component={Product} />
+      </div>
+    </Router>
   );
 };
 
